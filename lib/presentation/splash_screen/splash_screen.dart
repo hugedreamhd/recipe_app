a9e573ba49e8b7c36252701b9d03bf3a09abfec7
hugedreamhd <hugedreamhd@gmail.com>
2025-02-09 22:27:06 +0900

    feat : splash_screen -  column에 따른 'Get cooking' text가 화면 중앙 정렬이 되지 않아, TextAlign.center로 명시적으로 고정 - 화면 배경과 이미지를 asset에 넣고 imageAsset으로 설정

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/core/presentation/components/meduim_button.dart';
+
+import '../../ui/color_styles.dart';
+import '../../ui/text_styles.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+            'assets/images/splash_screen_image.png',
+          ),
+          Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 104,
+                ),
+                Image.asset(
+                  height: 79,
+                  width: 79,
+                  'assets/images/splash_screen_crown.png',
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  '100K+ Premium Recipe',
+                  style: TextStyles.mediumTextBold.copyWith(
+                    color: ColorStyles.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 275,
+                ),
+                SizedBox(
+                  width: 275,
+                  child: Column(
+                    children: [
+                      Text(
+                        textAlign: TextAlign.center,
+                        'Get Cooking',
+                        style: TextStyles.titleTextBold
+                            .copyWith(color: ColorStyles.white),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        textAlign: TextAlign.center,
+                        'Simple way to find Tasty Recipe',
+                        style: TextStyles.normalTextReguler.copyWith(
+                          color: ColorStyles.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 64,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 66),
+                  child: MediumButton(
+                    'Start Cooking',
+                    onPressed: () {},
+                  ),
+                ),
+                const SizedBox(
+                  height: 84,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
