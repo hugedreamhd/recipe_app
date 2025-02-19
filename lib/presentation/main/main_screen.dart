1880504ef4b680184d1a43b64a69e53d1ce7da01
Sangin Han <hugedreamhd@gmail.com>
2025-02-19 12:07:29 +0900

    feat: MainScreen 위젯 기본 구조 구현
    - MainScreen 위젯에 body 매개변수 추가하여 유연한 화면 구성 지원
    - 바텀 네비게이션 currentIndex 상태 관리 구현
    - 인덱스 변경을 위한 onChangeIndex 콜백 함수 추가
    
    세부 변경사항:
    - body: 메인 화면의 컨텐츠를 동적으로 변경할 수 있는 Widget 매개변수
    - currentIndex: 현재 선택된 바텀 네비게이션 인덱스 관리
    - onChangeIndex: 바텀 네비게이션 선택 변경 시 호출되는 콜백 함수

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+  final Widget body; //body 위젯을 받는다
+  final int currnetIndex; //현재 인덱스를 받는다
+  final void Function(int index)
+      onChangeIndex; //인덱스를 변경하는 콜백함수를 받는다(매개 변수 있을때는 void Fuction(매개변수) 형태로 받는다)
+
+  const MainScreen({
+    super.key,
+    required this.body,
+    required this.currnetIndex,
+    required this.onChangeIndex,
+  });
 
   @override
   Widget build(BuildContext context) {
