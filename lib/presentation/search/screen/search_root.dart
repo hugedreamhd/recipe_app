0641b9f3b3cdd6c36524b4cea74f0e7f3788b7ce
Sangin Han <hugedreamhd@gmail.com>
2025-03-09 16:21:46 +0900

    feat: SearchRoot에 LocalStorage 의존성 통합
    - DefaultLocalStorage 인스턴스를 전역 변수로 생성하여 공유
    - MockRecentSearchRecipeRepository에 LocalStorage 주입
    - SearchRecipesUseCase에 동일한 LocalStorage 인스턴스 주입
    - 단일 LocalStorage 인스턴스를 사용하여 데이터 일관성 보장
    - 의존성 주입 구조 개선으로 컴포넌트 간 데이터 공유 가능

--- lib/presentation/search/screen/search_root.dart
+++ lib/presentation/search/screen/search_root.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/data/data_source/local/local_recent_search_recipe_data_source_impl.dart';
+import 'package:recipe_app/data/data_source/local/default_local_storage.dart';
 import 'package:recipe_app/data/data_source/remote/remote_recipe_data_source_impl.dart';
 import 'package:recipe_app/data/repository/mock_recent_search_recipe_repository_impl.dart';
 import 'package:recipe_app/data/repository/mock_recipe_repository_impl.dart';
+import 'package:recipe_app/domain/data_source/local_storage.dart';
 import 'package:recipe_app/domain/use_case/search_recipes_use_case.dart';
 import 'package:recipe_app/presentation/search/screen/search_screen.dart';
 import 'package:recipe_app/presentation/search/search_view_model.dart';
 
+final LocalStorage _localStorage = DefaultLocalStorage();
+
 final _recentSearchRecipeRepository = MockRecentSearchRecipeRepositoryImpl(
-  recipeDataSource: LocalRecentSearchRecipeDataSourceImpl(),
+  localStorage: _localStorage,
 );
 
 class SearchRoot extends StatelessWidget {
@@ -22,6 +25,7 @@ class SearchRoot extends StatelessWidget {
         recipeRepository: MockRecipeRepositoryImpl(
           recipeDataSource: RemoteRecipeDataSourceImpl(),
         ),
+        localStorage: _localStorage,
       ),
     );
     return ListenableBuilder(
