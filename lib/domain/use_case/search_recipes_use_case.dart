b56144c8dafeb81acc58a35ea9c0ba356b2cee37
Sangin Han <hugedreamhd@gmail.com>
2025-03-09 16:20:14 +0900

    feat: LocalStorage를 활용한 검색 결과 저장 기능 구현
    - SearchRecipesUseCase에 LocalStorage 의존성 추가
    
    - 검색 결과를 LocalStorage에 저장하는 기능 구현
    
    - 검색된 레시피를 JSON 형태로 변환하여 저장
    
    - _localStorage.save({'recipes': results.map((e) => e.toJson())}) 코드 추가:
    
    - 검색 결과(results)를 map 함수로 순회하며 각 Recipe 객체를 JSON으로 변환
    
    - 'recipes' 키로 변환된 JSON 데이터를 LocalStorage에 저장
    
    - SearchRoot 클래스 업데이트
    
    - DefaultLocalStorage 인스턴스 생성 및 공유
    
    - MockRecentSearchRecipeRepository에 LocalStorage 주입
    
    - SearchRecipesUseCase에 LocalStorage 주입
    
    - 의존성 주입 구조 개선으로 데이터 일관성 유지

--- lib/domain/use_case/search_recipes_use_case.dart
+++ lib/domain/use_case/search_recipes_use_case.dart
@@ -1,17 +1,25 @@
+import 'package:recipe_app/domain/data_source/local_storage.dart';
 import 'package:recipe_app/domain/repository/recipe_repository.dart';
 
 import '../model/recipe.dart';
 
 class SearchRecipesUseCase {
   final RecipeRepository _recipeRepository;
+  final LocalStorage _localStorage;
 
   const SearchRecipesUseCase({
     required RecipeRepository recipeRepository,
-  }) : _recipeRepository = recipeRepository;
+    required LocalStorage localStorage,
+  })  : _recipeRepository = recipeRepository,
+        _localStorage = localStorage;
 
   Future<List<Recipe>> execute(String query) async {
     final results = (await _recipeRepository.getRecipes())
         .where((e) => e.name.toLowerCase().contains(query.toLowerCase()));
+
+    _localStorage.save({
+      'recipes': results.map((e) => e.toJson()),
+    });
 
     return results.toList();
   }
