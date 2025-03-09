3597b69098f7b6b7f0537ca3ee963a79e8028a4d
Sangin Han <hugedreamhd@gmail.com>
2025-03-09 17:24:00 +0900

    feat: SavedRecipesRoot에 GetIt 의존성 주입 적용
    getIt을 활용한 ViewModel 획득 방식으로 변경
    
    getIt<SavedRecipesViewModel>()을 통해 ViewModel 인스턴스 획득
    
    기존 직접 생성 방식에서 DI 컨테이너 활용 방식으로 전환
    
    의존성 주입 책임을 diSetup()으로 분리하여 코드 간결화
    
    ListenableBuilder 구조 유지하여 상태 변화에 따른 UI 업데이트 보장

--- lib/presentation/saved_recipes/screen/saved_recipes_root.dart
+++ lib/presentation/saved_recipes/screen/saved_recipes_root.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/data/data_source/remote/remote_recipe_data_source_impl.dart';
-import 'package:recipe_app/data/repository/mock_bookmart_repository_impl.dart';
-import 'package:recipe_app/data/repository/mock_recipe_repository_impl.dart';
-import 'package:recipe_app/domain/use_case/get_saved_recipes_use_case.dart';
+import 'package:recipe_app/core/di/di_setup.dart';
 import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
 import 'package:recipe_app/presentation/saved_recipes/screen/saved_recipes_screen.dart';
-
-final _getSavedRecipeUseCase = GetSavedRecipesUseCase(
-    recipeRepository: MockRecipeRepositoryImpl(
-      recipeDataSource: RemoteRecipeDataSourceImpl(),
-    ),
-    bookmarkRepository: MockBookmartRepositoryImpl());
 
 class SavedRecipesRoot extends StatelessWidget {
   const SavedRecipesRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final viewModel =
-        SavedRecipesViewModel(getSavedRecipeUseCase: _getSavedRecipeUseCase);
+    final viewModel = getIt<SavedRecipesViewModel>();
 
     return ListenableBuilder(
       listenable: viewModel,
