4a04e86987a253c2b34d421814edf70a9452c95e
Sangin Han <hugedreamhd@gmail.com>
2025-03-11 01:13:28 +0900

    feat: SearchRecipesUseCase에 필터링 기능 구현
    - execute 메서드에 FilterState 매개변수 추가
    
    - 다중 조건 필터링 로직 구현:
    
        - 시간(time) 필터링: 'All' 선택 시 모든 레시피 포함, 그 외에는 일치하는 항목만 필터링
        - 평점(rate) 필터링: 지정된 평점 이상의 레시피만 필터링
        - 카테고리(category) 필터링: 'All' 선택 시 모든 레시피 포함, 그 외에는 일치하는 항목만 필터링
    
    LocalStorage에 필터링된 결과 저장 기능 유지

--- lib/domain/use_case/search_recipes_use_case.dart
+++ lib/domain/use_case/search_recipes_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:recipe_app/domain/data_source/local_storage.dart';
+import 'package:recipe_app/domain/filter/filter_state.dart';
 import 'package:recipe_app/domain/repository/recipe_repository.dart';
 
 import '../model/recipe.dart';
@@ -13,9 +14,24 @@ class SearchRecipesUseCase {
   })  : _recipeRepository = recipeRepository,
         _localStorage = localStorage;
 
-  Future<List<Recipe>> execute(String query) async {
+  Future<List<Recipe>> execute(String query, FilterState filterState) async {
     final results = (await _recipeRepository.getRecipes())
-        .where((e) => e.name.toLowerCase().contains(query.toLowerCase()));
+        .where((e) => e.name.toLowerCase().contains(query.toLowerCase()))
+        .where((e) {
+          if (filterState.time == 'All') {
+            return true;
+          } else {
+            return filterState.time == e.time;
+          }
+        })
+        .where((e) => e.rating >= filterState.rate)
+        .where((e) {
+          if (filterState.category == 'All') {
+            return true;
+          } else {
+            return filterState.category == e.category;
+          }
+        });
 
     _localStorage.save({
       'recipes': results.map((e) => e.toJson()),
