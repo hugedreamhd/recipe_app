0b8cd173b48345a08f706e8bac210aabf5642f13
Sangin Han <hugedreamhd@gmail.com>
2025-03-10 15:37:21 +0900

    eat: SearchRoot에 GetIt 의존성 주입 적용
    getIt을 활용한 SearchViewModel 획득 방식으로 변경
    
    getIt<SearchViewModel>()을 통해 ViewModel 인스턴스 획득
    
    기존 직접 생성 방식에서 DI 컨테이너 활용 방식으로 전환
    
    의존성 주입 책임을 diSetup()으로 분리하여 코드 간결화
    
    ListenableBuilder 구조 유지하여 상태 변화에 따른 UI 업데이트 보장
    
    검색 기능 콜백 연결 방식 유지

--- lib/presentation/search/screen/search_root.dart
+++ lib/presentation/search/screen/search_root.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/data/data_source/local/default_local_storage.dart';
-import 'package:recipe_app/data/data_source/remote/remote_recipe_data_source_impl.dart';
-import 'package:recipe_app/data/repository/mock_recent_search_recipe_repository_impl.dart';
-import 'package:recipe_app/data/repository/mock_recipe_repository_impl.dart';
-import 'package:recipe_app/domain/data_source/local_storage.dart';
-import 'package:recipe_app/domain/use_case/search_recipes_use_case.dart';
 import 'package:recipe_app/presentation/search/screen/search_screen.dart';
 import 'package:recipe_app/presentation/search/search_view_model.dart';
 
-final LocalStorage _localStorage = DefaultLocalStorage();
-
-final _recentSearchRecipeRepository = MockRecentSearchRecipeRepositoryImpl(
-  localStorage: _localStorage,
-);
+import '../../../core/di/di_setup.dart';
 
 class SearchRoot extends StatelessWidget {
   const SearchRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = SearchViewModel(
-      recentSearchRecipeRepository: _recentSearchRecipeRepository,
-      searchRecipesUseCase: SearchRecipesUseCase(
-        recipeRepository: MockRecipeRepositoryImpl(
-          recipeDataSource: RemoteRecipeDataSourceImpl(),
-        ),
-        localStorage: _localStorage,
-      ),
-    );
+    final viewModel = getIt<SearchViewModel>();
+
     return ListenableBuilder(
       listenable: viewModel,
       builder: (context, widget) {
