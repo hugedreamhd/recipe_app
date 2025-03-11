4069c429ab347ded6197442d297727e0abb0acb3
Sangin Han <hugedreamhd@gmail.com>
2025-03-11 17:23:47 +0900

    refactor: HomeRoot에 상태 관리 및 네비게이션 기능 구현
    - GetIt을 활용한 HomeViewModel 의존성 주입
    - ListenableBuilder를 통한 상태 변화 감지 및 UI 업데이트 구현
    - 카테고리 선택 기능 연결:
    
        - viewModel.onSelectCategory 메서드 호출로 상태 업데이트
        - HomeScreen에 필요한 모든 콜백 함수 및 상태 전달

--- lib/presentation/home/screen/home_root.dart
+++ lib/presentation/home/screen/home_root.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:recipe_app/presentation/home/home_view_model.dart';
 import 'package:recipe_app/presentation/home/screen/home_screen.dart';
 
+import '../../../core/di/di_setup.dart';
 import '../../../core/routing/route_paths.dart';
 
 class HomeRoot extends StatelessWidget {
@@ -9,9 +11,20 @@ class HomeRoot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomeScreen(
-      name: 'Jega',
-      onTapSearchField: () => context.push(RoutePaths.search),
+    final viewModel = getIt<HomeViewModel>();
+
+    return ListenableBuilder(
+      builder: (context, snapshot) {
+        return HomeScreen(
+          state: viewModel.state,
+          name: 'Jega',
+          onTapSearchField: () => context.push(RoutePaths.search),
+          onSelectCategory: (String category) {
+            viewModel.onSelectCategory(category);
+          },
+        );
+      },
+      listenable: viewModel,
     );
   }
 }
