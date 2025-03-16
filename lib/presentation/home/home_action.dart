6a2d2c1feecf29178ca4211f6bc36cb25159d306
Sangin Han <hugedreamhd@gmail.com>
2025-03-16 23:59:35 +0900

    feat: HomeAction에 onTapFavorite 액션 추가
    Recipe 즐겨찾기 기능을 위한 onTapFavorite 액션 추가
    
    Recipe 모델을 파라미터로 받아 즐겨찾기 상태 변경 처리 가능
    
    MVI 패턴에 따라 사용자 의도를 명확하게 캡슐화
    
    HomeViewModel의 onAction 메서드에서 처리할 수 있는 액션 확장
    
    freezed를 활용한 타입 안전한 액션 처리 구조 유지

--- lib/presentation/home/home_action.dart
+++ lib/presentation/home/home_action.dart
@@ -1,9 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../domain/model/recipe.dart';
+
 part 'home_action.freezed.dart';
 
 @freezed
 sealed class HomeAction with _$HomeAction {
   const factory HomeAction.onTapSearchField() = OnTapSearchField;
   const factory HomeAction.onSelectCategory(String category) = OnSelectCategory;
+  const factory HomeAction.onTapFavorite(Recipe recipe) = OnTapFavorite;
 }
