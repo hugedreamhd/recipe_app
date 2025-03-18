97bf1af8656950fb1091942af39e40c9b330f544
Sangin Han <hugedreamhd@gmail.com>
2025-03-18 15:14:13 +0900

    add: BookmarkRepository에 실시간 북마크 상태 스트림 추가
    북마크 ID 변경을 실시간으로 구독할 수 있는 bookmarkIdsStream() 메서드 추가
    
    반응형 프로그래밍 패턴 도입으로 UI와 북마크 상태 동기화 개선

--- lib/domain/repository/bookmark_repository.dart
+++ lib/domain/repository/bookmark_repository.dart
@@ -1,7 +1,13 @@
 abstract interface class BookmarkRepository {
   Future<void> save(int id);
+
   Future<void> unSave(int id);
+
   Future<void> toggle(int id);
+
   Future<List<int>> getBookmarkIds();
+
+  Stream<Set<int>> bookmarkIdsStream();
+
   Future<void> clear();
 }
