f7f70804b353b31493903426cc3afa24ba52bd90
Sangin Han <hugedreamhd@gmail.com>
2025-03-18 15:12:42 +0900

    feat: MockBookmartRepositoryImpl에 실시간 북마크 상태 스트림 추가
    StreamController를 활용한 북마크 ID 변경 이벤트 발행 기능 구현
    
    bookmarkIdsStream() 메서드로 북마크 상태 변화를 실시간 구독 가능
    
    북마크 추가/제거/토글 시 자동으로 스트림에 최신 상태 전달
    
    broadcast 모드로 여러 구독자가 동시에 북마크 상태 변화 감지 가능

--- lib/data/repository/mock_bookmart_repository_impl.dart
+++ lib/data/repository/mock_bookmart_repository_impl.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
+
 import 'package:recipe_app/domain/repository/bookmark_repository.dart';
 
 class MockBookmartRepositoryImpl implements BookmarkRepository {
   final _ids = <int>{2, 4};
+  final _controller = StreamController<Set<int>>.broadcast();
+
+  MockBookmartRepositoryImpl() {
+    _controller.add(_ids);
+  }
 
   @override
   Future<void> clear() async {
@@ -16,19 +23,27 @@ class MockBookmartRepositoryImpl implements BookmarkRepository {
   @override
   Future<void> save(int id) async {
     _ids.add(id);
+    _controller.add(_ids);
   }
 
   @override
   Future<void> toggle(int id) async {
     if (_ids.contains(id)) {
-      unSave(id);
+      await unSave(id);
     } else {
-      save(id);
+      await save(id);
     }
+    _controller.add(_ids);
   }
 
   @override
   Future<void> unSave(int id) async {
     _ids.remove(id);
+    _controller.add(_ids);
+  }
+
+  @override
+  Stream<Set<int>> bookmarkIdsStream() {
+    return _controller.stream;
   }
 }
