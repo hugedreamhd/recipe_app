f40bcaab532e78da71e029574662645ed8ab3ea4
Sangin Han <hugedreamhd@gmail.com>
2025-03-24 18:07:36 +0900

    feat: 레시피 링크 공유 다이얼로그 컴포넌트 추가
    - 레시피 링크를 표시하고 복사할 수 있는 ShareDialog 위젯 구현
    - 링크 복사 기능을 위한 콜백 함수 추가

---
+++ lib/core/presentation/components/share_dialog.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/core/presentation/components/small_button.dart';
+
+import '../../../ui/color_styles.dart';
+import '../../../ui/text_styles.dart';
+
+class ShareDialog extends StatelessWidget {
+  final String link;
+  final void Function(String link) onTapCopyLink;
+
+  const ShareDialog({
+    super.key,
+    required this.link,
+    required this.onTapCopyLink,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 20,
+      ),
+      content: SizedBox(
+        width: 280,
+        child: Stack(
+          children: [
+            const Positioned(
+              right: 0,
+              child: Icon(
+                Icons.close,
+                size: 5,
+              ),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Recipe Link',
+                  style: TextStyles.largeTextBold,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Copy recipe link and share your recipe link with  friends and family.',
+                  style: TextStyles.smallTextReguler
+                      .copyWith(color: ColorStyles.gray3),
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      width: 280,
+                      height: 44,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9),
+                        color: ColorStyles.gray4,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 14,
+                          top: 13,
+                        ),
+                        child: Text(
+                          link,
+                          style: TextStyles.smallerTextBold,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      child: SizedBox(
+                        width: 85,
+                        height: 43,
+                        child: SmallButton(
+                          'Copy Link',
+                          onPressed: () => onTapCopyLink(link),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
